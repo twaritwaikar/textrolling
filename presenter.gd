609ce1831416a60818ca49e10b5f413f2caf0a8a
Twarit Waikar <wtwarit@gmail.com>
2024-08-16 01:38:53 +0530

    Functional build on PC

--- presenter.gd
+++ presenter.gd
@@ -8,11 +8,12 @@ func _ready():
 	label.text = $"/root/State".get_rolling_text()
 	label.label_settings.font_size = get_viewport_rect().size.y - 20
 	#label.label_settings.font_size = $"/root/State".get_rolling_text_size()
+	_reset_text()
 
 func _reset_text():
-	label.position.x = get_viewport_rect().size.x + label.size.x
+	label.position.x = get_window().size.x
 
 func _process(delta):
 	label.position.x -= $"/root/State".get_rolling_speed() * delta / 0.016
-	if label.position.x < -label.size.x:
+	if label.position.x + label.size.x < 0:
 		_reset_text()
