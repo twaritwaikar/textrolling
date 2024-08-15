cd78e4856b7ca76ea2f4da832975d3a59be97ca0
Twarit Waikar <wtwarit@gmail.com>
2024-08-16 01:00:55 +0530

    Make all text in fullscreen width

--- presenter.gd
+++ presenter.gd
@@ -3,15 +3,16 @@ extends Node2D
 var label : Label
 
 func _ready():
-	label = $VBoxContainer/RollingLabel
+	label = $RollingLabel
 	
 	label.text = $"/root/State".get_rolling_text()
-	label.label_settings.font_size = $"/root/State".get_rolling_text_size()
+	label.label_settings.font_size = get_viewport_rect().size.y - 20
+	#label.label_settings.font_size = $"/root/State".get_rolling_text_size()
 
 func _reset_text():
-	label.position.x = -label.size.x
+	label.position.x = get_viewport_rect().size.x + label.size.x
 
 func _process(delta):
-	label.position.x += $"/root/State".get_rolling_speed() * delta / 0.016
-	if label.position.x > get_viewport_rect().size.x:
+	label.position.x -= $"/root/State".get_rolling_speed() * delta / 0.016
+	if label.position.x < -label.size.x:
 		_reset_text()
